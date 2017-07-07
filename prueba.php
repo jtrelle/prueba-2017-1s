d65ac40eaab98f1251602f9dbcf470931ac9a975
Jenniffer Trelles <jtrelles@espol.edu.ec>
2017-07-07 13:21:27 -0500

    Update prueba.php
    changee

--- prueba.php
+++ prueba.php
@@ -1,8 +1,9 @@
+
 <?php
 class Test extends PHPUnit_Framework_TestCase
 {
-	public function testOnePlusOne() {
-		$this->assertEquals(1+1,1);
-  	}
+  public function testOnePlusOne() {
+    $this->assertEquals(1+1,2);
+    }
 }
 ?>
